71c3eb19bb39650463b4e7a7d4b7f2084232e5ec
xBluu <meteahmetyakar@gmail.com>
2022-07-06 01:06:39 +0300

    .

---
+++ problems/4.Find Pivot Index/solution.c
@@ -0,0 +1,18 @@
+int leftSum = 0, rightSum = 0, totalSum = 0;
+    
+    for(int i=0; i<numsSize; i++)
+        totalSum += nums[i];
+    
+    for(int i=0; i<numsSize; i++)
+    {        
+        if(i>0)
+            leftSum += nums[i-1];
+        
+        rightSum = totalSum-nums[i]-leftSum;
+        
+        if(leftSum == rightSum)
+            return i;
+            
+    }
+    
+    return -1;   
